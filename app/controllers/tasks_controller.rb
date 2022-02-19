cef92b63e397fd06c7cf1f3f0ae48d04867ddbe9
udaikue <ikueikue@gmail.com>
2022-02-19 22:09:16 +0900

    Implement new and create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,8 +6,21 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_path, notice: "「#{task.name}」を登録しました٩( 'ω' )و"
   end
 
   def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
