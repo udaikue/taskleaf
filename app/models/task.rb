a98959eb81c090f2fc3578e2c917379e16544b68
udaikue <ikueikue@gmail.com>
2022-03-11 09:57:34 +0900

    Use callback

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true
   validates :name, length: { maximum: 20 }
   validate :validate_name_not_including_comma
@@ -9,5 +11,9 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
   end
 end
