45db8cd59de14ddbd695c93105e5fbeed023d1c4
udaikue <ikueikue@gmail.com>
2022-03-16 09:39:12 +0900

    Change to deny using task manager without user log-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@
 
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_require
 
   private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_require
+    redirect_to login_url unless current_user
   end
 end
