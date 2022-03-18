67f091bdb103c037a20d213fa27ffff22eccee89
udaikue <ikueikue@gmail.com>
2022-03-18 09:14:36 +0900

    Add set_task method to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -22,22 +23,23 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_path, notice: "#{task.name}を更新しました( ・∇・)"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_path, notice: "#{task.name}を削除しましたϵ( 'Θ' )϶"
   end
 
   private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:name, :description)
