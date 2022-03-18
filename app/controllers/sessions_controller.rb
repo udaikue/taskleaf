049a711f069125bc46050b7b164040a1cb2519c5
udaikue <ikueikue@gmail.com>
2022-03-18 10:29:30 +0900

    Fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required
-  
+
   def new
   end
 
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destory
+  def destroy
     reset_session
     redirect_to root_url, notice: 'ログアウトしました'
   end
