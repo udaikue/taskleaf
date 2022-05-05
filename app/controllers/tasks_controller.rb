74bff56d88b26867f4b0267b8294501ab48ec9d4
udaikue <ikueikue@gmail.com>
2022-05-05 15:49:21 +0900

    Create display message job

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,7 @@ class TasksController < ApplicationController
 
     if @task.save
       TaskMailer.creation_email(@task).deliver_now
+      SampleJob.perform_later
       redirect_to @task, notice: "「#{@task.name}」を登録しました٩( 'ω' )و"
     else
       render :new
